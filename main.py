50b74a90d8bd127bb61234d86b753cb191e92541
Mayara Victoria Bezerra Nunes - SENAI - Cuiaba/MT <mayara.nunes@alunosenai.mt>
2026-07-24 11:21:38 -0400

    numero

--- main.py
+++ main.py
@@ -23,3 +23,7 @@ num1 = int(input("Digite um número: "))
 num2 = int(input("Digite outro número: "))
 soma = num1 + num2
 print(f"A soma dos dois números é: {soma}")
+
+numero = float(input("Digite um número para dobrar: "))
+dobro = numero * 2
+print(f"O dobro de {numero} é {dobro}")
